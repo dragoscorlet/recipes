3b519c050880db91b431ad58cf97ac19fcfc8ddb
dragos <dragos@DESKTOP-1II1T44>
2018-01-06 22:39:08 +0200

    add domain types

---
+++ Recipes/Recipes.Domain/Domain.fs
@@ -0,0 +1,32 @@
+﻿namespace Recipes.Domain
+
+open System;
+
+type Unit = Kilogram
+            |Gram
+            |Liter
+            |Deciliter
+            |Milliliter
+            |Cup
+            |TableSpoon
+            |DessertSpoon
+            |TeaSpoon
+            |Pound
+            |Ounce
+            |PintUs
+            |PingGB
+            |FluidOunceUS
+            |FluidOunceGB
+
+type Ingredient = {Name:string; Qty:int; Unit:Unit}
+
+type PictureLocation = {Thumbnail:Uri; FullSize:Uri}
+
+type Recipe = {
+    Name: string; 
+    Ingredients: Ingredient list; 
+    CookTimeMin: int; 
+    Servings: int; 
+    Picture: PictureLocation;
+    Description: string
+    Source: Uri}
