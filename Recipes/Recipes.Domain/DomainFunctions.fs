31d8b81113ffb9fc46bdfbe5f5f9d7fcf089ad08
dragos <dragos@DESKTOP-1II1T44>
2018-01-07 00:10:37 +0200

    add domain functions

---
+++ Recipes/Recipes.Domain/DomainFunctions.fs
@@ -0,0 +1,28 @@
+﻿
+module DomainFunctions
+open Recipes.Domain.DomainTypes;
+open System;
+
+let getHardcodedrecipe = 
+        let recipe = {
+        Name="dasdsad";
+        Ingredients = [{ Ingredient = Ingredient "";Qty = 2.0M; Unit= Unit.Cup}]
+        CookTimeMin = 40.0M;
+        Servings = 4;
+        Picture =  {Thumbnail = Uri(""); FullSize = Uri("") };
+        Description = "sfsf";
+        Source = Uri("");
+        }
+        recipe
+
+let getFixedIngredientsRecepies (ingredients:Ingredient seq) = 
+    let recepies = [getHardcodedrecipe]
+    recepies
+
+let getRecepies(ingredients: Ingredient seq) = 
+    let recipes = [getHardcodedrecipe]
+    recipes |> Seq.sortByDescending(fun r -> r.Ingredients.Length)
+
+
+
+
---
+++ Recipes/Recipes.Domain/DomainTypes.fs
@@ -0,0 +1,37 @@
+﻿namespace Recipes.Domain
+
+open System;
+
+[<AutoOpen>]
+module DomainTypes = 
+
+    type Unit = Kilogram
+                |Gram
+                |Liter
+                |Deciliter
+                |Milliliter
+                |Cup
+                |TableSpoon
+                |DessertSpoon
+                |TeaSpoon
+                |Pound
+                |Ounce
+                |PintUs
+                |PingGB
+                |FluidOunceUS
+                |FluidOunceGB
+
+    type Ingredient = Ingredient of string
+
+    type RecipeIngredient = {Ingredient: Ingredient; Qty:decimal; Unit:Unit}
+
+    type PictureLocation = {Thumbnail:Uri; FullSize:Uri}
+
+    type Recipe = {
+        Name: string; 
+        Ingredients: RecipeIngredient list; 
+        CookTimeMin: decimal; 
+        Servings: int; 
+        Picture: PictureLocation;
+        Description: string
+        Source: Uri}
